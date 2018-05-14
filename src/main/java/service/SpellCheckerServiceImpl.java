5f14bb0b5c718e6f108acfe2e1de941afec4c939
sudaredd <sudaredd@gmail.com>
2018-05-13 22:54:10 -0400

    merge commits

--- src/main/java/service/SpellCheckerServiceImpl.java
+++ src/main/java/service/SpellCheckerServiceImpl.java
@@ -2,9 +2,12 @@ package service;
 
 public class SpellCheckerServiceImpl implements SpellChecker {
 
+	public SpellCheckerServiceImpl() {
+		System.out.println("in constructor SpellCheckerServiceImpl");
+	}
+
 	@Override
 	public void checkSpelling() {
-
 		System.out.println("Inside check spelling");
 	}
 
